19b0daa684d6897439668da56c355b96456d82f0
Blankeos <carloantonioct@gmail.com>
2024-12-06 12:17:13 +0800

    fix: ci errors.

--- dev/lib/hono-client.ts
+++
@@ -1,5 +0,0 @@
-import type { AppRouter } from "@/server/_app";
-import { publicConfig } from "@/src/config.public";
-import { hc } from "hono/client";
-
-export const honoClient = hc<AppRouter>(`${publicConfig.BASE_ORIGIN}/api`);
